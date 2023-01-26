253873f5d7071fe79786a7e176c3186e9b881e1e
DarioGiusino <dario.giusino@gmail.com>
2023-01-26 16:45:39 +0100

    get banner image from api

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script>
 import axios from 'axios';
-import { apiUri, apiKey } from './data'
+import { apiUri, apiKey, imgUri } from './data'
 export default {
   name: 'Boolflix',
   data() {
     return {
       searchWord: '',
       moviesList: [],
-      showsList: []
+      showsList: [],
+      imgUri
     }
   },
   methods: {
-    // get movies by name from the API
+    // get movies&shows by name from the API
     fetchMovies() {
       // verify if input is not valid
       if (!this.searchWord) return
@@ -24,8 +25,8 @@ export default {
 
           // filter the result by giving a new array with selected keys
           const filteredMovies = movies.map((movie) => {
-            const { title, original_title, original_language, overview, vote_average } = movie
-            return { title, originalTitle: original_title, language: original_language, desc: overview, rating: vote_average }
+            const { title, original_title, original_language, overview, vote_average, poster_path } = movie
+            return { title, originalTitle: original_title, language: original_language, desc: overview, rating: vote_average, bannerUrl: poster_path }
           })
 
           //push the new array in data
@@ -41,15 +42,17 @@ export default {
 
           // filter the result by giving a new array with selected keys
           const filteredShows = shows.map((show) => {
-            const { name, original_name, original_language, overview, vote_average } = show
-            return { title: name, originalTitle: original_name, language: original_language, desc: overview, rating: vote_average }
+            const { name, original_name, original_language, overview, vote_average, poster_path } = show
+            return { title: name, originalTitle: original_name, language: original_language, desc: overview, rating: vote_average, bannerUrl: poster_path }
           })
 
           //push the new array in data
           this.showsList = filteredShows
         })
+        .catch(err => { console.error(err) });
     },
 
+    //build the url for the flag image
     renderFlag(lang) {
       const url = new URL(`./assets/img/flags/${lang}.png`, import.meta.url);
       return url.href;
@@ -65,10 +68,11 @@ export default {
     <button>Cerca</button>
   </form>
 
-  <!-- # Movies list -->
+  <!-- # Movies&Shows list -->
   <ul>
     <h1>----------------FILM---------------</h1>
     <li v-for="movie in moviesList" :key="movie.id">
+      <img :src="`${imgUri}${movie.bannerUrl}`" alt="">
       {{ movie.title }},
       {{ movie.originalTitle }},
       <span>lang: {{ movie.language }}</span>
@@ -83,6 +87,7 @@ export default {
     <br>
     <h1>----------------SERIES---------------</h1>
     <li v-for="show in showsList" :key="show.id">
+      <img :src="`${imgUri}${show.bannerUrl}`" alt="">
       {{ show.title }},
       {{ show.originalTitle }},
       <span>lang: {{ show.language }}</span>
--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,5 @@
 const apiUri = "https://api.themoviedb.org/3";
 const apiKey = "9702acf11fe2f1447113210f74754f56";
+const imgUri = "https://image.tmdb.org/t/p/w342/";
 
-export { apiUri, apiKey };
+export { apiUri, apiKey, imgUri };
