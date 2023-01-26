5cd6678571713427f31e9aa7c4e04803fdc15435
DarioGiusino <dario.giusino@gmail.com>
2023-01-26 14:46:27 +0100

    created input text, button and linked to data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script>
 export default {
   name: 'Boolflix',
+  data() {
+    return {
+      searchWord: ''
+    }
+  }
 }
 </script>
 
 <template>
-
+  <!-- # Search Bar -->
+  <form>
+    <input type="text" placeholder="Cerca..." v-model.trim="searchWord">
+    <button type="button">Cerca</button>
+  </form>
 </template>
 
 <style>
@@ -13,5 +22,6 @@ body {
   background-color: #0d1117;
   color: #f0f6fc;
   min-height: 100vh;
+  padding: 20px;
 }
 </style>
